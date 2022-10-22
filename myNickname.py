5e6b696bc5a185d2fa77977a0931b0488f47b69e
Adrian Esguerra <i.am.adriansgrrx@gmail.com>
2022-10-22 12:29:58 +0800

    I started working on my nickname program by drawing the pattern of my nickname which is EDS. P.S.: I started immediately and this is not a real time commit. This is for documentation purposes only

---
+++ myNickname.py
@@ -0,0 +1,20 @@
+# Assignment 1:
+# Create a program that will print your nickname using only asterisk character (*)
+
+# Hello Sir. Dan! My nickname is Eds!
+# I will be using for loops and if statements for this program
+
+# This pattern will be my reference
+# * * * *  * * *     * * * 
+# *        *    *   *  
+# *        *     *  *   
+# * * *    *     *   * * * 
+# *        *     *        *   
+# *        *    *         *   
+# * * * *  * * *     * * * 
+
+
+
+
+
+
